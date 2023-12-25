c7dd8272afe360235818b1cf0bcfc2b219fe5ef0
phollakrit <66252619+phollakrit@users.noreply.github.com>
2023-12-25 11:25:51 +0700

    Update App.js css inline
    Update App.js css inline

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react'
 
 function App() {
-  let r='<b>React</b>'
-  let rn='React Native'
-  const el=(
-    <div>
-      Create Web & Mobile Apps<br/>
-      wiht <span dangerouslySetInnerHTML={{__html: r}}/> & {rn}
-    </div>
-  )
-  return el
+  const divStyle={
+    color:'black',
+    backgroundColor:'powderblue',
+    fontSize:'larger',
+    padding:'10px',
+  }
+  return <div style={divStyle}>Hello React</div>
 }
 
 export default App;
