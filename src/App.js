25df68222f8e60a3d8705622976bafa9f8a2bc66
phollakrit <66252619+phollakrit@users.noreply.github.com>
2024-02-19 09:51:10 +0700

    Create form-search.js
    Create form-search.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import Router3 from './router3'
+import FormSearch from './form-search'
 
 export default function App() {
-  return <Router3/>
+  return <FormSearch/>
 }
 
 
