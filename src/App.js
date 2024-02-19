87955914d0d537c2852bb6ee6d73fd6c1f354988
phollakrit <66252619+phollakrit@users.noreply.github.com>
2024-02-19 11:21:24 +0700

    Create form-text.js
    Create form-text.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import FormSearch from './form-search'
+import FormText from './form-text'
 
 export default function App() {
-  return <FormSearch/>
+  return <FormText/>
 }
 
 
